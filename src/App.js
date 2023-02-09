a4ab0c5715bd3e640722d35437e36a2c81b8e283
G Hatzi <52458189+Ghatzi@users.noreply.github.com>
2023-02-09 17:53:49 +0000

    update to router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Layout from './Layout';
+import Missing from './Missing';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+
 function App() {
-  return <div className="App">working</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="post">
+          <Route index element={<NewPost />} />
+          <Route path=":id" element={<PostPage />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
