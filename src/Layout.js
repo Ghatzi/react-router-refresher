71fdff20f60c87a5164e665eb08d0015df73d6ce
G Hatzi <52458189+Ghatzi@users.noreply.github.com>
2023-02-09 23:23:39 +0000

    create posts

--- src/Layout.js
+++ src/Layout.js
@@ -3,11 +3,11 @@ import Footer from './Footer';
 import Header from './Header';
 import Nav from './Nav';
 
-const Layout = () => {
+const Layout = ({ search, setSearch }) => {
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Outlet />
       <Footer />
     </div>
--- src/NewPost.js
+++ src/NewPost.js
@@ -1,7 +1,32 @@
-const NewPost = () => {
+const NewPost = ({
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+  handleSubmit
+}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          value={postTitle}
+          onChange={e => setPostTitle(e.target.value)}
+          required
+        />
+
+        <label htmlFor="postBody">Body:</label>
+        <textarea
+          id="postBody"
+          value={postBody}
+          onChange={e => setPostBody(e.target.value)}
+          required
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 };
